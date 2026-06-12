93d40f28ed92028cc7917a61b4bd7e6f5fe8927d
qianwanbie <lby241757@gmail.com>
2026-06-12 08:47:07 +0800

    Remove accidentally committed local docs and temp files

--- fix_bashrc.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const bashrc = '/data/data/com.termux/files/home/.bashrc';
-const line = "export CLAUDE_CODE_TMPDIR=$HOME/.tmp\n";
-let content = fs.readFileSync(bashrc, 'utf-8');
-content = content.split('\n').filter(l => !l.includes('CLAUDE_CODE_TMPDIR')).join('\n');
-fs.writeFileSync(bashrc, content.trim() + '\n' + line);
-console.log('OK');
